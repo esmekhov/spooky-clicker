fc77055be1c43fd4b1d9d735761cb2343fd4f09b
esmekhov <egor.smekhov@gmail.com>
2017-10-14 19:55:12 +0300

    Removed non-webpack test scaffold

--- test/specs/core.specs.js
+++ test/specs/core.specs.js
@@ -1,12 +1,5 @@
-/* global Greeter */
 describe('Core', function() {
-  it('Greeter exsists', () => expect(Greeter).is.not.undefined )
-
-  it('it can send friendly messages', () => {
-    var greeter = new Greeter()
-    expect(greeter.message).is.equal('hi there Dear Coder!')
-    // these white spaces will be trimmed
-    greeter.message = '   goodbye         '
-    expect(greeter.message).is.equal('goodbye Dear Coder!')
+  it('jquery is there', () => {
+    expect(jQuery).is.not.equal(undefined)
   })
 })
